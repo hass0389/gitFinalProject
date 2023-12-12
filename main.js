c45cdb7e94d120b5778e1cfcdedb434211beac49
Fuad <fuad@Fuads-MacBook-Pro.local>
2023-12-12 14:37:18 -0500

    Uploading files

---
+++ main.js
@@ -0,0 +1,35 @@
+let numArray = [1, 4, 3, 4, 23, 6, 17, 8, 19, 10,]
+
+for (let i = 0; i < numArray.length; i++){
+    console.log(numArray.length / 10);
+    return numArray
+}
+
+// let date = new Date(2023-12-25)
+// console.log(date);
+
+// const todayDate =  new Date().toLocaleString();
+
+
+// getDaysUntilChristmas(todayDate) {
+//     console.log(todayDate)
+    
+// }
+// let date = new Date();
+// let month = date.getMonth() + 1;
+// let chrisDate = new
+
+// function getDaysUntilChristmas() {
+//     switch (date) {
+//         case 1:
+//             console.log("This is Today")
+//             break;
+//         case 2:
+//             console.log("This is until christmas")
+//             break;
+//     }
+//     return (date - )
+// }
+
+// let myMonth = getDaysUntilChristmas()
+// console.log(myMonth)
